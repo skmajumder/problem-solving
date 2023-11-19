6e65c9b815006cc2b7cad187f75de51f14b9819c
skmajumder <skmajumder.cse@gmail.com>
2023-11-19 22:05:29 +0600

    problem 1 - Closure

--- leetcode_d_1/index.js
+++ leetcode_d_1/index.js
@@ -6,3 +6,24 @@ const createHelloWorld = function () {
 
 const f = createHelloWorld();
 console.log(f());
+
+// * closure
+// function createCounter() {
+//   let value = 0; // * Hidden state
+
+//   function increment() {
+//     return ++value;
+//   }
+
+//   return {
+//     increment,
+//   };
+// }
+
+// const counter = createCounter();
+// const counter2 = createCounter();
+
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter2.increment());
